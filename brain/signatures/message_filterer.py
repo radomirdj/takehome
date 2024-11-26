6e1ca46abf904f38942f515d5324561a6c8693d1
Radomir Djokovic <radomir.djokovic@yara.com>
2024-11-26 23:59:28 +0100

    Small fixes in prompts

--- brain/signatures/message_filterer.py
+++ brain/signatures/message_filterer.py
@@ -10,9 +10,10 @@ class MessageFilterer(dspy.Signature):
     * They must not mention social media platforms (except OnlyFans)
     * They should never suggest in-person meetings with fans.
     You will receive the message and return message with filtered out this part.
+    If  there's no need for filtering don't write that, but just return message as is.
     """
 # Maybe it could happend if whole message is filtered out that we send empty message - to be improved
-    creator_message: str = dspy.InputField(desc="Message that OnlyFans creator has written ant that needs tobe validated")
+    creator_message: str = dspy.InputField(desc="Message that OnlyFans creator has written ant that needs to be validated")
 
     output: str = dspy.OutputField(
         desc="creators message with filtered out problematic parts.",
